1ab30f1938a94a834103d49dd79a7093d3266e69
OscarAR46 <oscar.a-r@hotmail.com>
2025-04-04 12:03:53 +0100

    Practice folder

---
+++ Pokemon_Rock_Paper_Scissors.py
@@ -0,0 +1,134 @@
+Charmander = '''
+              _.--""`-..
+            ,'          `.
+          ,'          __  `.
+         /|          " __   \
+        , |           / |.   .
+        |,'          !_.'|   |
+      ,'             '   |   |
+     /              |`--'|   |
+    |                `---'   |
+     .   ,                   |                       ,".
+      ._     '           _'  |                    , ' \ `
+  `.. `.`-...___,...---""    |       __,.        ,`"   L,|
+  |, `- .`._        _,-,.'   .  __.-'-. /        .   ,    \
+-:..     `. `-..--_.,.<       `"      / `.        `-/ |   .
+  `,         """"'     `.              ,'         |   |  ',,
+    `.      '            '            /          '    |'. |/
+      `.   |              \       _,-'           |       ''
+        `._'               \   '"\                .      |
+           |                '     \                `._  ,'
+           |                 '     \                 .'|
+           |                 .      \                | |
+           |                 |       L              ,' |
+           `                 |       |             /   '
+            \                |       |           ,'   /
+          ,' \               |  _.._ ,-..___,..-'    ,'
+         /     .             .      `!             ,j'
+        /       `.          /        .           .'/
+       .          `.       /         |        _.'.'
+        `.          7`'---'          |------"'_.'
+       _,.`,_     _'                ,''-----"'
+   _,-_    '       `.     .'      ,\
+   -" /`.         _,'     | _  _  _.|
+    ""--'---"""""'        `' '! |! /
+                            `" " -' 
+'''
+Squirtle = '''
+               _,........__
+            ,-'            "`-.
+          ,'                   `-.
+        ,'                        \
+      ,'                           .
+      .'\               ,"".       `
+     ._.'|             / |  `       \
+     |   |            `-.'  ||       `.
+     |   |            '-._,'||       | \
+     .`.,'             `..,'.'       , |`-.
+     l                       .'`.  _/  |   `.
+     `-.._'-   ,          _ _'   -" \  .     `
+`."""""'-.`-...,---------','         `. `....__.
+.'        `"-..___      __,'\          \  \     \
+\_ .          |   `""""'    `.           . \     \
+  `.          |              `.          |  .     L
+    `.        |`--...________.'.        j   |     |
+      `._    .'      |          `.     .|   ,     |
+         `--,\       .            `7""' |  ,      |
+            ` `      `            /     |  |      |    _,-'"""`-.
+             \ `.     .          /      |  '      |  ,'          `.
+              \  v.__  .        '       .   \    /| /              \
+               \/    `""\"""""""`.       \   \  /.''                |
+                `        .        `._ ___,j.  `/ .-       ,---.     |
+                ,`-.      \         ."     `.  |/        j     `    |
+               /    `.     \       /         \ /         |     /    j
+              |       `-.   7-.._ .          |"          '         /
+              |          `./_    `|          |            .     _,'
+              `.           / `----|          |-............`---'
+                \          \      |          |
+               ,'           )     `.         |
+                7____,,..--'      /          |
+                                  `---.__,--.'mh
+'''
+Bulbasaur = '''
+                                           /
+                        _,.------....___,.' ',.-.
+                     ,-'          _,.--"        |
+                   ,'         _.-'              .
+                  /   ,     ,'                   `
+                 .   /     /                     ``.
+                 |  |     .                       \.\
+       ____      |___._.  |       __               \ `.
+     .'    `---""       ``"-.--"'`  \               .  \
+    .  ,            __               `              |   .
+    `,'         ,-"'  .               \             |    L
+   ,'          '    _.'                -._          /    |
+  ,`-.    ,".   `--'                      >.      ,'     |
+ . .'\'   `-'       __    ,  ,-.         /  `.__.-      ,'
+ ||:, .           ,'  ;  /  / \ `        `.    .      .'/
+ j|:D  \          `--'  ' ,'_  . .         `.__, \   , /
+/ L:_  |                 .  "' :_;                `.'.'
+.    ""'                  """""'                    V
+ `.                                 .    `.   _,..  `
+   `,_   .    .                _,-'/    .. `,'   __  `
+    ) \`._        ___....----"'  ,'   .'  \ |   '  \  .
+   /   `. "`-.--"'         _,' ,'     `---' |    `./  |
+  .   _  `""'--.._____..--"   ,             '         |
+  | ." `. `-.                /-.           /          ,
+  | `._.'    `,_            ;  /         ,'          .
+ .'          /| `-.        . ,'         ,           ,
+ '-.__ __ _,','    '`-..___;-...__   ,.'\ ____.___.'
+ `"^--'..'   '-`-^-'"--    `-^-'`.''"""""`.,^.`.--'
+'''
+import random
+
+pokemon_dict = {
+    "Charmander": Charmander,
+    "Squirtle": Squirtle,
+    "Bulbasaur": Bulbasaur
+}
+
+
+pokemon_trainer_name = input("Here come Team Rocket, which Pokémon will you use to beat them? Charmander, Squirtle, or Bulbasaur? ").strip().capitalize()
+pokemon_trainer_art = pokemon_dict.get(pokemon_trainer_name)
+
+if pokemon_trainer_art is None:
+    print("Invalid choice! Go Again!")
+    exit()
+
+
+team_rocket_art = random.choice(list(pokemon_dict.values()))
+
+print(pokemon_trainer_art, "\nI choose you!")
+print("Team Rocket chose:", team_rocket_art)
+
+
+if pokemon_trainer_art == team_rocket_art:
+    print("Your Pokémon knocked each other out! I wonder who will win next time?")
+elif (
+    (pokemon_trainer_art == Charmander and team_rocket_art == Bulbasaur) or
+    (pokemon_trainer_art == Squirtle and team_rocket_art == Charmander) or
+    (pokemon_trainer_art == Bulbasaur and team_rocket_art == Squirtle)
+):
+    print("Let's go!", pokemon_trainer_name, "just obliterated", [name for name, art in pokemon_dict.items() if art == team_rocket_art][0] + " They're crying like little babies!")
+else:
+    print("Oh no, looks like Team Rocket got the best of you this time... ")
